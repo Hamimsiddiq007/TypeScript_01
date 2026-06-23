cdedaa693009f2de88f190f62d214cf171f38ac3
Hamim <hamimsiddiq915@gmail.com>
2026-06-23 22:56:47 +0600

    types in ts

---
+++ src/typesInTs.ts
@@ -0,0 +1,12 @@
+let drink = "tea"
+
+// drink = 123  //Invalid
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let teaFlavour:string = "Masala tea"
+
+teaFlavour = "Green tea"
+
+let teaOrder:boolean = true;
+
